c81febfbb61b51cb38e8955ab73ee97062f88b75
kuomin <tw.kuomin@gmail.com>
2022-08-09 20:35:37 +0800

    Connect Api data

--- game.js
+++ game.js
@@ -1,76 +1,3 @@
-const data = [
-  {
-    type: '詐騙',
-    level: 1,
-    title: '以下何者沒有犯法?',
-    item: [
-      {
-        id: '1',
-        name: '擔任詐騙集團之車手',
-        description: '1台中一年輕人王小明(化名)，近日前往倍兒好廣告公司應聘網紅，被要求押金10萬保證金才得以入職接受教育',
-        law: '依洗錢防制法第15條之規定，收受、持有或使用之財物或財產上利益，有下列情形之一，而無合理來源且與收入顯不相當者，處6月以上5年以下有期徒',
-        ans: false,
-      },
-      {
-        id: '2',
-        name: '擔任詐騙集團之車手',
-        description: '2台中一年輕人王小明(化名)，近日前往倍兒好廣告公司應聘網紅，被要求押金10萬保證金才得以入職接受教育',
-        law: '依洗錢防制法第15條之規定，收受、持有或使用之財物或財產上利益，有下列情形之一，而無合理來源且與收入顯不相當者，處6月以上5年以下有期徒',
-        ans: false,
-      },
-      {
-        id: '3',
-        name: '擔任詐騙集團之水手',
-        description: '',
-        law: '',
-        ans: true,
-      },
-      {
-        id: '4',
-        name: '擔任詐騙集團之車手',
-        description: '4台中一年輕人王小明(化名)，近日前往倍兒好廣告公司應聘網紅，被要求押金10萬保證金才得以入職接受教育',
-        law: '依洗錢防制法第15條之規定，收受、持有或使用之財物或財產上利益，有下列情形之一，而無合理來源且與收入顯不相當者，處6月以上5年以下有期徒',
-        ans: false,
-      },
-    ],
-  },
-  {
-    type: '詐騙',
-    level: 2,
-    title: '以下何者沒有犯法?',
-    item: [
-      {
-        id: '5',
-        name: '替「別人」的犯罪所得漂白',
-        description: '台中一年輕人王小明(化名)，替「別人」的犯罪所得漂白',
-        law: '依洗錢防制法第2條，本法所稱洗錢，指下列行為：一、意圖掩飾或隱匿特定犯罪所得來源，或使他人逃',
-        ans: false,
-      },
-      {
-        id: '6',
-        name: '替「別人」的犯罪所得漂白',
-        description: '台中一年輕人王小明(化名)，替「別人」的犯罪所得漂白',
-        law: '依洗錢防制法第2條，本法所稱洗錢，指下列行為：一、意圖掩飾或隱匿特定犯罪所得來源，或使他人逃',
-        ans: false,
-      },
-      {
-        id: '7',
-        name: '替「別人」的犯罪所得漂白',
-        description: '台中一年輕人王小明(化名)，替「別人」的犯罪所得漂白',
-        law: '依洗錢防制法第2條，本法所稱洗錢，指下列行為：一、意圖掩飾或隱匿特定犯罪所得來源，或使他人逃',
-        ans: false,
-      },
-      {
-        id: '8',
-        name: '替「別人」的犯罪出庭自白',
-        description: '',
-        law: '',
-        ans: true,
-      },
-    ],
-  },
-];
-
 const itemarea = document.querySelector('.itemarea');
 const itemBtn = document.querySelector('.item');
 const contanier = document.querySelector('.contanier');
@@ -90,18 +17,129 @@ const gamemanError = 'https://media.giphy.com/media/AWxZgaOyDZFwJu7YiZ/giphy.gif
 
 let gamelevel = 1;
 
-// 先預設詐騙
-let gametype = '詐騙';
+// 正確動畫
+function correctAnimate(e) {
+  e.target.classList.add('correct');
+  itemarea.classList.add('animate__backOutDown', 'animate__delay-1s');
+}
 
-// 畫面選項渲染
-function render(type, level, title, item) {
+// 匯入下一關動畫
+function nextAnimate() {
+  itemarea.classList.remove('animate__backOutDown', 'animate__delay-1s');
+  itemBtn.classList.remove('correct');
+  itemarea.classList.add('animate__backInDown');
+}
+
+
+// 取得遊戲關卡種類
+function getcategories(){
+  axios({
+    method: 'get',
+    baseURL: 'https://money-laundry.herokuapp.com/v1/api/categories',
+    timeout: 2000,
+  })
+  .then((result) => {
+    let categories = result.data.data;
+    rendercategories(categories);
+
+      // 按鈕關卡判斷邏輯
+    itemarea.addEventListener('click', (e) => {
+      if (e.target.nodeName === 'BUTTON') {
+        const { id } = e.target.dataset;
+        categories.forEach((i) => {
+          if (i.id == id) {            
+            correctAnimate(e);
+            setTimeout((() => nextAnimate()), 1500);
+            toString(i.id);
+            setTimeout((() => getgamequestion(i.id,gamelevel)), 1500);
+          }
+        });
+      }
+      e.preventDefault();
+    },true);
+
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+
+// 取得特定種類關卡題目
+function getgamequestion(id, level) {
+  axios({
+    method: 'get',
+    baseURL: `https://money-laundry.herokuapp.com/v1/api/categories/${id}`,
+    timeout: 2000,
+  })
+  .then((result) => {
+    newsTitle.textContent = `${result.data.data.type}反受害 洗錢詐騙新招`;
+    let gamequestion = result.data.data.Games;
+    renderquestion(gamequestion,level);
+    const gameid = gamequestion[`${level}`-1].id;
+    getgameitems(gameid);
+  })
+}
+
+// 取得特定種類關卡選項
+function getgameitems(gameid) {
+  axios({
+    method: 'get',
+    baseURL: `https://money-laundry.herokuapp.com/v1/api/games/${gameid}`,
+    timeout: 2000,
+  })
+  .then((result) => {
+    let gameitems = result.data.data.Items;
+    let gameid = result.data.data.Category.id;
+    renderitems(gameitems);
+
+    // 按鈕關卡判斷邏輯
+    itemarea.addEventListener('click', (e) => {
+      if (e.target.nodeName === 'BUTTON') {
+        const { id } = e.target.dataset;
+        gameitems.forEach((i) => {
+          if (i.id == id) {
+            if (i.ans) {
+              correctAnimate(e);
+              if (gamelevel === 5) {
+                console.log('win');
+              } else {
+                gamelevel += 1;
+                setTimeout((() => nextAnimate()), 1500);
+                setTimeout((() => getgamequestion(gameid,gamelevel)), 1000);
+              }
+            }
+          }
+        });
+      }
+    });
+  })
+}
+
+
+// 關卡種類畫面渲染
+function rendercategories(arr) {
   let str = '';
-  item.forEach((i) => {
-    str += `<button class="item dashed"  data-id="${i.id}">${i.name}</button>`;
+  arr.forEach((i) => {
+    str += `<button class="item dashed"  data-id="${i.id}">${i.type}</button>`;
   });
   itemarea.innerHTML = str;
-  titletext.textContent = `Q${level}:${title}`;
-  newsTitle.textContent = `${type}反受害 洗錢詐騙新招`;
+  titletext.textContent = `請選擇遊戲關卡`;
+}
+
+// 關卡題目畫面渲染
+function renderquestion(arr,num) {
+  const question = arr.filter((i) => i.level === num );
+  titletext.textContent = `Q${question[0].level}:${question[0].title}`;
+}
+
+// 關卡選項畫面渲染
+function renderitems(arr) {
+  let str = '';
+  arr.forEach((i) => {
+    str += `<button class="item dashed"  data-id="${i.id}">${i.name}</button>`;
+  }
+  );
+  itemarea.innerHTML = str;
 }
 
 // 取得日期
@@ -129,8 +167,12 @@ function chooseData(num, str) {
 
 // 遊戲開始
 function start() {
-  chooseData(gamelevel, gametype);
+  getcategories();
   getdate();
+  setTimeout((() => renderstart()), 1000);
+}
+// 遊戲開始動畫
+function renderstart() {
   contanier.classList.add('d-flex');
   Gamestart.classList.add('d-none');
   itemarea.classList.remove('d-none');
@@ -138,14 +180,6 @@ function start() {
   titletext.classList.add('text-start');
   man.classList.remove('w600');
   man.style.backgroundImage = `url(${gamemanImage})`;
-}
-
-// 晉級下一關
-function next(num, str) {
-  chooseData(num, str);
-  itemarea.classList.remove('animate__backOutDown', 'animate__delay-1s');
-  itemBtn.classList.remove('correct');
-  itemarea.classList.add('animate__backInDown');
 }
 
 // 回到主遊戲
@@ -164,29 +198,3 @@ function reset() {
 // 監聽按鈕
 Gamestart.addEventListener('click', start);
 Gamesreset.addEventListener('click', reset);
-
-// 按鈕關卡判斷邏輯
-itemarea.addEventListener('click', (e) => {
-  if (e.target.nodeName === 'BUTTON') {
-    const { id } = e.target.dataset;
-    let TypeData = data.find((i) => gametype === i.type && gamelevel === i.level);
-    TypeData.item.forEach((i) => {
-      if (i.id === id) {
-        if (i.ans) {
-          e.target.classList.add('correct');
-          itemarea.classList.add('animate__backOutDown', 'animate__delay-1s');
-          gamelevel += 1;
-          setTimeout((() => render(gamelevel, gametype)), 1500);
-          setTimeout((() => next(gamelevel, gametype)), 1500);
-          TypeData = [];
-        } else {
-          e.target.classList.add('error');
-          man.style.backgroundImage = `url(${gamemanError})`;
-          newsArea.classList.remove('d-none');
-          newsDescription.textContent = `${i.description}`;
-          newsLaw.textContent = `${i.law}`;
-        }
-      }
-    });
-  }
-});
